568e166d22a0b2761783910ab8d97be26c539539
Meikochow <victorcojocaru93@gmail.com>
2023-04-29 17:08:07 -0500

    ran predeploy

---
+++ docs/precache-manifest.95fa83d80c5d908218b73c2075740af4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bdaf67d9f1c0da320de8",
+    "url": "/static/js/main.bdaf67d9.chunk.js"
+  },
+  {
+    "revision": "8974caf372232c5c65cc",
+    "url": "/static/js/1.8974caf3.chunk.js"
+  },
+  {
+    "revision": "bdaf67d9f1c0da320de8",
+    "url": "/static/css/main.1f10ecbf.chunk.css"
+  },
+  {
+    "revision": "8974caf372232c5c65cc",
+    "url": "/static/css/1.9dd27f46.chunk.css"
+  },
+  {
+    "revision": "f8875c7a795e8e565666fc91f191754b",
+    "url": "/index.html"
+  }
+];
