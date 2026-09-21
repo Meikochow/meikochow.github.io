89284542817c19a614607de340c3379121d43fca
Meikochow <victorcojocaru93@gmail.com>
2026-09-21 16:24:42 -0500

    deploy (predeploy ran

--- docs/precache-manifest.e8dbfcdfc651a34121b46e0fe23a1580.js
+++ docs/precache-manifest.e8dbfcdfc651a34121b46e0fe23a1580.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "4998b1a76c93f5aa69e9",
-    "url": "/static/js/main.4998b1a7.chunk.js"
+    "revision": "b7bf1c1003a974ae74fe",
+    "url": "/static/js/main.b7bf1c10.chunk.js"
   },
   {
     "revision": "15fb8ac854b60b4aaf1e",
     "url": "/static/js/1.15fb8ac8.chunk.js"
   },
   {
-    "revision": "4998b1a76c93f5aa69e9",
-    "url": "/static/css/main.bde26b9d.chunk.css"
+    "revision": "b7bf1c1003a974ae74fe",
+    "url": "/static/css/main.fc65a862.chunk.css"
   },
   {
     "revision": "15fb8ac854b60b4aaf1e",
     "url": "/static/css/1.149b6ba3.chunk.css"
   },
   {
-    "revision": "a333cbc40b0d3f5a50a065ef510b0006",
+    "revision": "c1077dd369b71ff7058a0e3748ae52a5",
     "url": "/index.html"
   }
 ];
